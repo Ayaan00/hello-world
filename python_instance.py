99549cdfb7b2af02e8a10682b264faa2ca5a6f14
Ayaan00 <48843681+Ayaan00@users.noreply.github.com>
2019-11-23 16:32:07 -0500

    Update python_instance.py

--- python_instance.py
+++ python_instance.py
@@ -3,7 +3,7 @@ ec2 = boto3.resource('ec2')
 
 # create a new EC2 instance
 instances = ec2.create_instances(
-     ImageId='ami-00dc79254d0461090',
+     ImageId='ami-00068cd7555f543d5',
      MinCount=1,
      MaxCount=1,
      InstanceType='t2.micro',
